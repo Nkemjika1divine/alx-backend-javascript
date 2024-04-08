442578e390658c993392ec9642603625954af35e
Nkemjika1divine <cndivine@gmail.com>
2024-04-08 21:45:00 +0000

    ES6 Basics

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   let result = "";
-  let employee = reportWithIterator.next();
+let employee = reportWithIterator.next();
   while (!employee.done)
   {
     result += employee.value;
