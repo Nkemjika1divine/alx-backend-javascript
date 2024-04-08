898b6469a7956537809f2ac1602555aab6834bf7
Nkemjika1divine <cndivine@gmail.com>
2024-04-08 21:54:41 +0000

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let emp = [];
-for (const [department, employees] of Object.entries(report)) {
-  for (const [key, value] of Object.entries(employees)){
-    for (let b of value){
-      emp.push(b);
-    }
-  }
-}
-let iterEmp = emp[Symbol.iterator]();
-return iterEmp;
+  return Object.values(report.allEmployees).flat();
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = "";
-let employee = reportWithIterator.next();
-  while (!employee.done)
-  {
-    result += employee.value;
-    employee = reportWithIterator.next();
-    if (!employee.done)
-      result += (" | ");
-  }
-  return result;
+  return reportWithIterator.join(' | ');
 }
